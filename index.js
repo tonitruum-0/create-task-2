727f784fa1672be654384934a72fabb0167921d7
tonitruum <mslesarevich2@gmail.com>
2023-02-16 19:12:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,40 @@
 const DOMSelectors = {
-  numField: document.getElementById("numberField"),
-  submitBtn: document.getElementById("submit"),
+  numField: document.getElementById('numberField'),
+  submitBtn: document.getElementById('submit'),
 };
 
 let result;
 
-DOMSelectors.submitBtn.addEventListener("click", NaNcheck);
+DOMSelectors.submitBtn.addEventListener('click', NaNcheck);
 
 function NaNcheck(e) {
   e.preventDefault();
+  let num = DOMSelectors.numField.value;
   if (isNaN(DOMSelectors.numField.value)) {
-    document.body.innerHTML += "pls enter a number";
-    DOMSelectors.numField.value = "";
+    document.body.innerHTML += 'pls enter a number';
+    DOMSelectors.numField.value = '';
   } else {
-    isEven(DOMSelectors.numField.value);
+    let evenOdd = isEven(num);
+    let fact = numFact(num);
   }
 }
 
-async function isEven(num) {
+function isEven(num) {
+  if (num % 2 === 0) {
+    return 'even';
+  } else {
+    return 'odd';
+  }
+}
+
+async function numFact(num) {
   try {
-    let api_url = `https://api.isevenapi.xyz/api/iseven/${num}/`;
+    let api_url = `http://numbersapi.com/${num}`;
     let response = await fetch(api_url);
     result = await response.json();
-    document.body.innerHTML += result.iseven;
+    console.log(result);
   } catch (error) {
-    alert("Error caught: check console");
+    alert('Error caught: check console');
     console.error(error);
   }
 }
