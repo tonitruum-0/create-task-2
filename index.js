c14b419bbfe6db2f01e45e5e43d1ed7a8a40209b
tonitruum <mslesarevich2@gmail.com>
2023-02-17 13:13:48 -0500

    asdasd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const DOMSelectors = {
-    numField: document.getElementById('numberField'),
-    submitBtn: document.getElementById('submit'),
-    err: document.getElementById('err'),
+  numField: document.getElementById("numberField"),
+  submitBtn: document.getElementById("submit"),
+  err: document.getElementById("err"),
 };
 
 let result;
@@ -9,84 +9,78 @@ let arr = [];
 let fact;
 let i = 0;
 
-DOMSelectors.submitBtn.addEventListener('click', NaNcheck);
-function NaNcheck(e) {
-    e.preventDefault();
-    let num = DOMSelectors.numField.value;
-    if (num === '') {
-        add();
-        return;
+DOMSelectors.submitBtn.addEventListener("click", main);
+function main(e) {
+  e.preventDefault();
+  let num = DOMSelectors.numField.value;
+  if (num === "" || num < 1) {
+    add();
+    return;
+  } else {
+    remove();
+    if (isNaN(DOMSelectors.numField.value)) {
+      add();
     } else {
-        if (isNaN(DOMSelectors.numField.value)) {
-            add();
-        } else {
-            let evenOdd = isEven(num);
-            numFact(num)
-                .then(function (response) {
-                    arr.push(new array(num, evenOdd, response));
-                    createCards();
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
-            DOMSelectors.numField.value = '';
-        }
+      let evenOdd = isEven(num);
+      numFact(num)
+        .then(function (response) {
+          arr.push(new array(num, evenOdd, response));
+          createCards();
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+      DOMSelectors.numField.value = "";
     }
+  }
 }
 
 function isEven(num) {
-    if (num % 2 === 0) {
-        return 'even';
-    } else {
-        return 'odd';
-    }
+  if (num % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
 }
 
 async function numFact(num) {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', `http://numbersapi.com/${num}`);
-        xhr.onload = function () {
-            if (xhr.status === 200) {
-                resolve(xhr.responseText);
-            } else {
-                reject(
-                    new Error(
-                        'Request failed. Returned status of ' + xhr.status
-                    )
-                );
-            }
-        };
-        xhr.onerror = function () {
-            reject(new Error('Request failed. Network error'));
-        };
-        xhr.send();
-    });
+  try {
+    let api_url = `http://numbersapi.com/${num}?json`;
+    let response = await fetch(api_url);
+    result = await response.json();
+    return result.text;
+  } catch (error) {
+    alert("Error caught: check console");
+    console.error(error);
+  }
 }
 
 class array {
-    constructor(num, evenOdd, fact) {
-        this.Number = num;
-        this.EvenOrOdd = evenOdd;
-        this.Fact = fact;
-    }
+  constructor(num, evenOdd, fact) {
+    this.Number = num;
+    this.EvenOrOdd = evenOdd;
+    this.Fact = fact;
+    this.Created = "false";
+  }
 }
 
 function createCards() {
-    document.getElementById(
-        'container'
-    ).innerHTML += `<div class=card><p class="num text">${arr[i].Number} - ${arr[i].EvenOrOdd}</p><p class="fact text">${arr[i].Fact}</p></div>`;
-    i++;
+  arr.forEach((a) => {
+    if (a.Created === "false") {
+      a.Created = "true";
+      document.getElementById("container").insertAdjacentHTML("afterbegin", `<div class=card><p class="num text">${a.Number} - ${a.EvenOrOdd}</p><p class="fact text">${a.Fact}</p></div>`);
+    }
+  });
 }
 
 function add() {
-    DOMSelectors.err.style.opacity = '1';
-    DOMSelectors.err.style.transition = '1s ease';
-    DOMSelectors.err.addEventListener('transitionend', remove);
-    DOMSelectors.numField.value = '';
+  DOMSelectors.err.style.opacity = "1";
+  DOMSelectors.err.style.transition = "1s ease";
+  DOMSelectors.err.addEventListener("transitionend", remove);
+  DOMSelectors.numField.value = "";
 }
 
 function remove() {
-    DOMSelectors.err.style.opacity = '0';
-    DOMSelectors.err.style.transition = '1s ease';
+  DOMSelectors.err.style.opacity = "0";
+  DOMSelectors.err.style.transition = "1s ease";
 }
